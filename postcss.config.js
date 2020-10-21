8e076a7026258f3b0d775a1c7bd41f434ed51475
Roman Gurinovich <gurinovich.rv@gmail.com>
2020-10-21 08:12:46 +0300

    fix(postcss): исправить импорт плагинов
    Jira issue: VEGA-432

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-const createPostcssConfig = require('@gpn-prototypes/frontend-configs/postcss.config');
+// ref https://github.com/gpn-prototypes/vega-auth/commit/c0e41589049d99cd63e6616bae937a13aea014f0
+const nested = require('postcss-nested');
 
 module.exports = {
-  // eslint-disable-next-line global-require
-  plugins: [...createPostcssConfig().plugins],
+  plugins: [nested()],
 };
